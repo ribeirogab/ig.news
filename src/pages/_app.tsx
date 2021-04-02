103b04aa97aafef7937750804b8d7a24a7f45931
ribeirogab <gblosr@gmail.com>
2021-04-02 18:06:40 -0300

    feat: authenticate with GitHub added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import { AppProps } from 'next/app';
+import { Provider as NextAuthProvider } from 'next-auth/client';
 
 import '../styles/global.scss'
 import { Header } from '../components/Header';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   )
 }
